c5dc650c927558889a49ec27b0345285cf27b98b
vamsi reddy <60885542+vamsi10reddy@users.noreply.github.com>
2025-06-29 18:11:08 -0500

    Update demo.py
    updated demo.py for pylint action.

--- demo.py
+++ demo.py
@@ -1,8 +1,13 @@
+"""demo.py - Simple demo to add two numbers and return the result."""
+
+
 def myfunction():
+    """Adds two fixed integers and returns the result."""
     a = 2
     b = 4
     c = a + b
     return c
 
 
-myfunction()
+if __name__ == "__main__":
+    myfunction()
